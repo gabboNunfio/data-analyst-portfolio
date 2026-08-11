f21ef18fedf9d7f093ee06353df9a9cacdd98dd5
Abbner Gabriel Nunfio <gabbo.nunfio@gmail.com>
2026-08-12 01:08:49 +0200

    Configure Python data analysis environment

---
+++ scripts/environment_test.py
@@ -0,0 +1,11 @@
+import sys
+
+import numpy as np
+import pandas as pd
+
+
+print("Python:", sys.version)
+print("Executable:", sys.executable)
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Environment OK")
